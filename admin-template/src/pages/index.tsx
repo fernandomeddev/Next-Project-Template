2431d1b89d51a00e8316fd02d22471a08cdc51a8
Fernando Medeiros <fernandohenrique520@live.com>
2023-03-15 21:00:15 -0300

    firt commit

---
+++ admin-template/src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+     <h1 className="flex justify-center items-center text-3xl font-bold underline bg-gradient-to-r from-green-500 to-blue-500 h-screen">
+      Hello world!
+    </h1>
+    </>
+  )
+}
